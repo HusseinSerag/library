2ad4686e78676d18625d126a82aa92e427625ca3
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-11-19 14:44:04 +0200

    Finish navbar css and js transition

--- library.js
+++ library.js
@@ -14,18 +14,43 @@ function DisplayBooks(){
 
 
 let menuChange = document.querySelector('.menu')
+let navBackground = document.querySelector('.nav-background')
+let container = document.querySelector('.container')
+let nav = document.querySelector('.nav')
 const MENU_SRC = '/images/menu.svg';
 const MENU_CHANGE_SRC = '/images/menu-close.svg';
 
-menuChange.addEventListener('click',()=>{
+menuChange.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    changeMenuIcon()
+    
+})
+nav.addEventListener('click',(e)=>{
+    e.stopPropagation()
+})
+container.addEventListener('click',(e)=>{
+    if(nav.classList.contains('active')){
+        
+        navBackground.classList.remove('active')
+        nav.classList.remove('active')
+        changeMenuIcon()
+    }
+});
+
+
+function changeMenuIcon(){
     let currentMenu = menuChange.getAttribute('src');
     if( currentMenu.includes(MENU_SRC) )
-    {
+    {   navBackground.classList.add('active')
+        nav.classList.add('active')
         currentMenu = currentMenu.replace(MENU_SRC,MENU_CHANGE_SRC)
+        
     }
     else{
+        navBackground.classList.remove('active')
+        nav.classList.remove('active')
         currentMenu = currentMenu.replace(MENU_CHANGE_SRC,MENU_SRC)
     }
    
     menuChange.src = currentMenu
-})
+}
