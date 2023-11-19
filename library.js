582c53561569dac6cfa6c540518998f1749ba439
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2023-11-19 13:26:08 +0200

    Implement navbar icon navbar icon change onclick

--- library.js
+++ library.js
@@ -10,3 +10,22 @@ function addBookToLibrary(book){
 function DisplayBooks(){
 
 }
+
+
+
+let menuChange = document.querySelector('.menu')
+const MENU_SRC = '/images/menu.svg';
+const MENU_CHANGE_SRC = '/images/menu-close.svg';
+
+menuChange.addEventListener('click',()=>{
+    let currentMenu = menuChange.getAttribute('src');
+    if( currentMenu.includes(MENU_SRC) )
+    {
+        currentMenu = currentMenu.replace(MENU_SRC,MENU_CHANGE_SRC)
+    }
+    else{
+        currentMenu = currentMenu.replace(MENU_CHANGE_SRC,MENU_SRC)
+    }
+   
+    menuChange.src = currentMenu
+})
